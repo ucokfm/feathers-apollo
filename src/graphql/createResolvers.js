6fdd201c5f0cc3f5bca3e48dea693fe133df3ffa
Ucok Freddy <ucokfm@gmail.com>
2018-06-13 18:28:14 +0700

    Set provider and headers to resolvers

--- src/graphql/createResolvers.js
+++ src/graphql/createResolvers.js
@@ -19,46 +19,46 @@ module.exports = function (app) {
     },
     Query: {
       products: async (parent, args, context, info) => {
-        return (await productService.find()).data;
+        return (await productService.find({ provider: 'rest', headers: context.request.headers })).data;
       },
       product: async (parent, args, context, info) => {
-        return productService.get(args.id);
+        return productService.get(args.id, { provider: 'rest', headers: context.request.headers });
       },
       users: async (parent, args, context, info) => {
-        return (await userService.find()).data;
+        return (await userService.find({ provider: 'rest', headers: context.request.headers })).data;
       },
       user: async (parents, args, context, info) => {
-        return await userService.get(args.id);
+        return await userService.get(args.id, { provider: 'rest', headers: context.request.headers });
       },
     },
     Mutation: {
       createProduct: async (parent, args, context, info) => {
-        return productService.create(args);
+        return productService.create(args, { provider: 'rest', headers: context.request.headers });
       },
       updateProduct: async (parent, args, context, info) => {
         const { id, ...others } = args;
-        return productService.update(id, others);
+        return productService.update(id, others, { provider: 'rest', headers: context.request.headers });
       },
       patchProduct: async (parent, args, context, info) => {
         const { id, ...others } = args;
-        return productService.patch(id, others);
+        return productService.patch(id, others, { provider: 'rest', headers: context.request.headers });
       },
       removeProduct: async (parent, args, context, info) => {
-        return productService.remove(args.id);
+        return productService.remove(args.id, { provider: 'reset' });
       },
       createUser: async (parent, args, context, info) => {
-        return userService.create(args);
+        return userService.create(args, { provider: 'rest', headers: context.request.headers });
       },
       updateUser: async (parent, args, context, info) => {
         const { id, ...others } = args;
-        return userService.update(id, others);
+        return userService.update(id, others, { provider: 'rest', headers: context.request.headers });
       },
       patchUser: async (parent, args, context, info) => {
         const { id, ...others } = args;
-        return userService.patch(id, others);
+        return userService.patch(id, others, { provider: 'rest', headers: context.request.headers });
       },
       removeUser: async (parent, args, context, info) => {
-        return userService.remove(args.id);
+        return userService.remove(args.id, { provider: 'rest', headers: context.request.headers });
       },
     },
   };
