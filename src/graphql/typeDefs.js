adc15f8ebd3698235bb94b4d3d59b9071adbf7da
Ucok Freddy <ucokfm@gmail.com>
2018-11-01 16:42:44 +0700

    Remove commas from typeDefs

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -2,24 +2,24 @@ const { gql } = require('apollo-server');
 
 module.exports = gql`
   type Product {
-    id: Int,
-    name: String,
+    id: Int
+    name: String
     price: Int
     owner: User
   }
 
   type User {
-    id: Int,
-    fullName: String,
+    id: Int
+    fullName: String
     dateOfBirth: String
     products: [Product]
   }
 
   type Query {
-    products: [Product],
-    product(id: Int): Product,
+    products: [Product]
+    product(id: Int): Product
     users: [User]
-    user(id: Int): User,
+    user(id: Int): User
   }
 
   type Mutation {
