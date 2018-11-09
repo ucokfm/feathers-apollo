8d2a5631cabe16eba21d9add7296db2c53522611
Ucok Freddy <ucokfm@gmail.com>
2018-11-09 17:15:44 +0700

    Remove unused files for monorepo

--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-const users = require('./users/users.service.js');
-const products = require('./products/products.service.js');
-// eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
-  app.configure(users);
-  app.configure(products);
-};
